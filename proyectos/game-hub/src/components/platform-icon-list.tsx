08850c2e51fa62611248da83475c70fe51d5fa25
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-15 10:15:13 -0300

    Lista de plataformas con íconos

---
+++ proyectos/game-hub/src/components/platform-icon-list.tsx
@@ -0,0 +1,46 @@
+import { Platform2 } from "@/types/games";
+import { Icon, HStack } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+  FaLinux,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface PlatformIconListProps {
+  platforms: Platform2[];
+}
+
+export const PlatformIconList = ({ platforms }: PlatformIconListProps) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    web: BsGlobe,
+  };
+
+  if (platforms.length === 0) return null;
+
+  return (
+    <HStack marginY={"10px"}>
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color={"gray.500"}
+        />
+      ))}
+    </HStack>
+  );
+};
