db1dbdcbd3e18cf87539bede35808c5c9152b7f9
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-15 10:34:30 -0300

    Optimizar imagen mediante crop

---
+++ proyectos/game-hub/src/services/image-service.ts
@@ -0,0 +1,8 @@
+export const getCroppedImageUrl = (
+  url: string,
+  width: number,
+  height: number,
+  target = "media/",
+) => {
+  return url.replace(target, `${target}crop/${width}/${height}/`);
+};
