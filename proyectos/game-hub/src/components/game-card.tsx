ae4194b499ed43583b8e86a62e9573719d654183
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-15 10:15:59 -0300

    Uso de la lista de íconos en la tarjeta del juego

--- proyectos/game-hub/src/components/game-card.tsx
+++ proyectos/game-hub/src/components/game-card.tsx
@@ -1,14 +1,20 @@
 import { Game } from "@/types/games";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { PlatformIconList } from "./platform-icon-list";
 
 interface GameCardProps extends Game {}
 
-export const GameCard = ({ name, background_image }: GameCardProps) => {
+export const GameCard = ({
+  name,
+  background_image,
+  parent_platforms,
+}: GameCardProps) => {
   return (
     <Card borderRadius={10} overflow={"hidden"}>
       <Image src={background_image} />
       <CardBody>
         <Heading fontSize={"2xl"}>{name}</Heading>
+        <PlatformIconList platforms={parent_platforms.map((p) => p.platform)} />
       </CardBody>
     </Card>
   );
