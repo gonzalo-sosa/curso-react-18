dc96bc2cbf5f8ae1efbe44610628cc744fd7bb9f
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-16 11:15:19 -0300

    Uso de datos estáticos para evitar llamadas a la api por datos que no cambian

--- proyectos/game-hub/src/hooks/useGenres.ts
+++ proyectos/game-hub/src/hooks/useGenres.ts
@@ -1,7 +1,9 @@
-import { Genre } from '@/types/genre';
-import genresService from '@/services/genres-service';
-import useData from './useData';
+// import { Genre } from '@/types/genre';
+// import genresService from '@/services/genres-service';
+// import useData from './useData';
+import genres from '@/data/genres';
 
-const useGenres = () => useData<Genre>(genresService, []);
+// const useGenres = () => useData<Genre>(genresService, []);
+const useGenres = () => ({ data: genres, error: null, isLoading: false });
 
 export default useGenres;
--- proyectos/game-hub/src/hooks/usePlatforms.ts
+++ proyectos/game-hub/src/hooks/usePlatforms.ts
@@ -1,7 +1,13 @@
-import { Platform } from '@/types/platform';
-import useData from './useData';
-import platformsService from '@/services/platforms-service';
+// import { Platform } from '@/types/platform';
+// import useData from './useData';
+// import platformsService from '@/services/platforms-service';
+import parent_platforms from '@/data/parent_platforms';
 
-const usePlatforms = () => useData<Platform>(platformsService, []);
+// const usePlatforms = () => useData<Platform>(platformsService, []);
+const usePlatforms = () => ({
+  data: parent_platforms,
+  error: null,
+  isLoading: false,
+});
 
 export default usePlatforms;
