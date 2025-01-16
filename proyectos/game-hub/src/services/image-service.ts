d3c4894cb4ea173527c30fdf09c6923bf64116fe
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-16 10:22:24 -0300

    Mostrar imagen genérica para juegos sin imagen

--- proyectos/game-hub/src/services/image-service.ts
+++ proyectos/game-hub/src/services/image-service.ts
@@ -1,10 +1,12 @@
+import noImage from '@/assets/no-image-placeholder.webp';
+
 export const getCroppedImageUrl = (
   url: string,
   width: number,
   height: number,
   target = 'media/',
 ) => {
-  if (!url) return '';
+  if (!url) return noImage;
 
   return url.replace(target, `${target}crop/${width}/${height}/`);
 };
