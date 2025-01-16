76e00ef211df9ef6ea0e8f49e33ab6070606886a
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-16 10:18:29 -0300

    Parámetro de params solucionado

--- proyectos/game-hub/src/hooks/useGames.ts
+++ proyectos/game-hub/src/hooks/useGames.ts
@@ -6,7 +6,7 @@ import { GameQuery } from '@/types/query';
 const useGames = (gameQuery: GameQuery) => {
   const urlParams = new URLSearchParams();
   Object.entries(gameQuery).forEach(([key, value]) => {
-    if (value) urlParams.append(key, value.id);
+    if (value) urlParams.append(key, value.id ?? value);
   });
 
   return useData<Game>(gamesService(urlParams), [gameQuery]);
