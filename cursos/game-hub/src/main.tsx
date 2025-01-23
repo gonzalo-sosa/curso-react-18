5403ad946a04bef05cc8b6f7761bd65f829824eb
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-23 15:40:00 -0300

    Añadido react query

--- cursos/game-hub/src/main.tsx
+++ cursos/game-hub/src/main.tsx
@@ -2,11 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Provider from '@/components/ui/provider';
 import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <App />
+      </Provider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>,
 );
