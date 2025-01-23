076c3074f74e09e880294ec2858674c1abec4321
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-23 14:26:27 -0300

    Copia

---
+++ cursos/intermediate/src/react-query/PostList.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import usePosts from './hooks/usePosts';
+
+const PostList = () => {
+  const [userId, setUserId] = useState<number>();
+  const { data: posts, error, isLoading } = usePosts(userId);
+
+  if (isLoading)
+    return (
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <>
+      <select
+        onChange={(e) => setUserId(parseInt(e.target.value))}
+        value={userId}
+        className="form-select mb-3"
+      >
+        <option value="" selected disabled></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default PostList;
