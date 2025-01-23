aa9d01931032a586c09efc05a7062f372b44e37e
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-23 14:28:05 -0300

    Uso de infinite query

---
+++ cursos/intermediate/src/react-query/PostListWithInfiniteQueries.tsx
@@ -0,0 +1,47 @@
+import { Fragment } from 'react/jsx-runtime';
+import usePosts from './hooks/usePosts';
+
+interface PostListWithInfiniteQueriesProps {
+  pageSize?: number;
+}
+
+const PostListWithInfiniteQueries = ({
+  pageSize = 10,
+}: PostListWithInfiniteQueriesProps) => {
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
+
+  if (isLoading)
+    return (
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <>
+      <ul className="list-group">
+        {data.pages.map((page, index) => (
+          <Fragment key={`page-${index}`}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </Fragment>
+        ))}
+      </ul>
+      <button
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}
+        className="btn btn-primary ms-3 my-3"
+      >
+        {isFetchingNextPage ? 'Loading...' : 'Load More'}
+      </button>
+    </>
+  );
+};
+
+export default PostListWithInfiniteQueries;
