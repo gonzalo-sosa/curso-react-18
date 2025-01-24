3d99acc9c91571cc1b305bc07d85e9b29a942cee
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-24 09:45:51 -0300

    Añadidos tipos de datos a la instancia de http service de tipo genre

--- cursos/game-hub/src/services/genres-service.ts
+++ cursos/game-hub/src/services/genres-service.ts
@@ -1,6 +1,8 @@
 import config from '@/config';
 import { create } from './http-service';
+import { Genre } from '@/types/genre';
+import { ApiResponse } from '@/types/api';
 
-export default create(
+export default create<ApiResponse<Genre>>(
   'https://api.rawg.io/api/genres' + `?key=${config.api_key}`,
 );
