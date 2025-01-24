538173f88fbd54b6771c423c85875e01ecbdb8ba
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-24 09:44:45 -0300

    Actualización de imports

--- cursos/intermediate/src/react-query/hooks/usePosts.ts
+++ cursos/intermediate/src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import axios, { AxiosRequestConfig } from 'axios';
 
 interface Post {
