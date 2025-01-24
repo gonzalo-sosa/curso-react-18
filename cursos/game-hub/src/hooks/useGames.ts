308fbabcad5c858e29111ebb89882d6b3cf77dd8
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-24 10:56:12 -0300

    Hook actualizado para utilizar react query

--- cursos/game-hub/src/hooks/useGames.ts
+++ cursos/game-hub/src/hooks/useGames.ts
@@ -1,15 +1,36 @@
 import { Game } from '@/types/games';
 import gamesService from '@/services/games-service';
-import useData from './useData';
 import { GameQuery } from '@/types/query';
+import { useQuery } from '@tanstack/react-query';
+import { ApiResponse } from '@/types/api';
 
-const useGames = (gameQuery: GameQuery) => {
+// const useGames = (gameQuery: GameQuery) => {
+//   const urlParams = new URLSearchParams();
+//   Object.entries(gameQuery).forEach(([key, value]) => {
+//     if (value) urlParams.append(key, value.id ?? value);
+//   });
+
+//   return useData<Game>(gamesService(urlParams), [gameQuery]);
+// };
+
+// () =>
+//       fetch(`https://api.rawg.io/api/games?key=${config.api_key}`).then(
+//         (data) => data.json(),
+//       ),
+
+function toURLParams(gameQuery: GameQuery) {
   const urlParams = new URLSearchParams();
   Object.entries(gameQuery).forEach(([key, value]) => {
     if (value) urlParams.append(key, value.id ?? value);
   });
 
-  return useData<Game>(gamesService(urlParams), [gameQuery]);
-};
+  return urlParams;
+}
+
+const useGames = (gameQuery: GameQuery) =>
+  useQuery<ApiResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: () => gamesService(toURLParams(gameQuery)).getAll().response,
+  });
 
 export default useGames;
