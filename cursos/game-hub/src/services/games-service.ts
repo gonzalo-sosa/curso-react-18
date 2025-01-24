7099c99b1b00ff9fc3a3b1a7ccf929c5a7912526
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-24 10:54:07 -0300

    Service con tipo estricto

--- cursos/game-hub/src/services/games-service.ts
+++ cursos/game-hub/src/services/games-service.ts
@@ -1,11 +1,13 @@
+import { ApiResponse } from '@/types/api';
 import { create } from './http-service';
 import config from '@/config';
+import { Game } from '@/types/games';
 
 const gamesService = (params?: URLSearchParams) => {
   const endpoint = new URL('https://api.rawg.io/api/games');
   if (params) endpoint.search = `?key=${config.api_key}&${params.toString()}`;
-
-  return create(endpoint.toString());
+  else endpoint.search = `?key=${config.api_key}`;
+  return create<ApiResponse<Game>>(endpoint.toString());
 };
 
 export default gamesService;
