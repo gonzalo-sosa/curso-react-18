7d30809b295d3dba89c776bf58cd10835519d9ab
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-24 15:06:33 -0300

    Tipo de dato actualizado

--- cursos/game-hub/src/types/query.d.ts
+++ cursos/game-hub/src/types/query.d.ts
@@ -2,8 +2,8 @@ import { Genre } from './genre';
 import { ParentPlatform } from './platform';
 
 export interface GameQuery {
-  genres: Genre | null;
-  parent_platforms: ParentPlatform | null;
+  genres?: Genre['id'];
+  parent_platforms?: ParentPlatform['id'];
   ordering: string;
   search: string;
   page?: number;
