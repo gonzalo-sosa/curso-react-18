3e4172f877442bcb7f7702ddb97c1af8f9a11ee9
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-24 09:46:40 -0300

    Hook con opciones para evitar peticiones con react query

--- cursos/game-hub/src/hooks/useGenres.ts
+++ cursos/game-hub/src/hooks/useGenres.ts
@@ -1,9 +1,24 @@
 // import { Genre } from '@/types/genre';
 // import genresService from '@/services/genres-service';
 // import useData from './useData';
-import genres from '@/data/genres';
+// import genres from '@/data/genres';
+
+import genresService from '@/services/genres-service';
+import { ApiResponse } from '@/types/api';
+import { Genre } from '@/types/genre';
+import { useQuery } from '@tanstack/react-query';
 
 // const useGenres = () => useData<Genre>(genresService, []);
-const useGenres = () => ({ data: genres, error: null, isLoading: false });
+// const useGenres = () => ({ data: genres, error: null, isLoading: false });
+
+const useGenres = () =>
+  useQuery<ApiResponse<Genre>, Error>({
+    queryKey: ['genres'],
+    queryFn: () => genresService.getAll().response,
+    staleTime: Infinity, // para evitar un fetch por data nueva
+    retry: 1,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
 
 export default useGenres;
