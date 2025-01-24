34cc2872ca40c45a096ec5d6e5a36cc11dd6557a
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-24 10:23:11 -0300

    Actualización del hook para utilizar react query y datos iniciales

--- cursos/game-hub/src/hooks/usePlatforms.ts
+++ cursos/game-hub/src/hooks/usePlatforms.ts
@@ -1,13 +1,34 @@
 // import { Platform } from '@/types/platform';
 // import useData from './useData';
 // import platformsService from '@/services/platforms-service';
+
 import parent_platforms from '@/data/parent_platforms';
+import platformsService from '@/services/platforms-service';
+import { ApiResponse } from '@/types/api';
+import { ParentPlatform } from '@/types/platform';
+import { useQuery } from '@tanstack/react-query';
 
 // const usePlatforms = () => useData<Platform>(platformsService, []);
-const usePlatforms = () => ({
-  data: parent_platforms,
-  error: null,
-  isLoading: false,
-});
+// const usePlatforms = () => ({
+//   data: parent_platforms,
+//   error: null,
+//   isLoading: false,
+// });
+
+const usePlatforms = () =>
+  useQuery<ApiResponse<ParentPlatform>, Error>({
+    queryKey: ['platforms'],
+    queryFn: () => platformsService.getAll().response,
+    initialData: {
+      count: parent_platforms.length,
+      previous: null,
+      next: null,
+      results: parent_platforms as ParentPlatform[],
+    },
+    staleTime: Infinity,
+    retry: 1,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
 
 export default usePlatforms;
