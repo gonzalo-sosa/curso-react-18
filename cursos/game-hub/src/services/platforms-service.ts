f0cdd1a1051fa3994599efe2b0518904518129c6
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-24 10:22:37 -0300

    Añadido tipos de datos estrictos para instancia de http service

--- cursos/game-hub/src/services/platforms-service.ts
+++ cursos/game-hub/src/services/platforms-service.ts
@@ -1,6 +1,8 @@
+import { ApiResponse } from '@/types/api';
 import { create } from './http-service';
 import config from '@/config';
+import { ParentPlatform } from '@/types/platform';
 
-export default create(
+export default create<ApiResponse<ParentPlatform>>(
   'https://api.rawg.io/api/platforms/lists/parents' + `?key=${config.api_key}`,
 );
