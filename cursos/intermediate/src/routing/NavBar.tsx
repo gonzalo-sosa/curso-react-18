01d36bbc3a2a06935aedfefde7f5d8039795d34c
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-26 20:01:05 -0300

    Añadido react router dom

---
+++ cursos/intermediate/src/routing/NavBar.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{ background: '#f0f0f0', marginBottom: '1rem' }}
+    >
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          My App
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/users">
+                Users
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
