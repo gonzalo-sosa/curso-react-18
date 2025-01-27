e144dc4c3af5ca2bb275303632cad1f01217e2e7
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-27 14:27:31 -0300

    Error de juegos sin plataformas solucionado. Gracias agus por ser el tester <3

--- cursos/game-hub/src/components/game-card.tsx
+++ cursos/game-hub/src/components/game-card.tsx
@@ -1,5 +1,5 @@
-import { Game } from '@/types/games';
-import { Card, CardBody, Heading, Image, HStack } from '@chakra-ui/react';
+import Game from '@/types/games';
+import { Card, CardBody, Heading, Image, HStack, Text } from '@chakra-ui/react';
 import PlatformIconList from './platform-icon-list';
 import CriticScore from './critic-score';
 import { getCroppedImageUrl } from '@/services/image-service';
@@ -18,14 +18,22 @@ const GameCard = ({
 }: GameCardProps) => {
   return (
     <Card as={Link} to={`/games/${slug}`}>
-      <Image src={getCroppedImageUrl(background_image, 600, 400)} />
+      <Image
+        src={getCroppedImageUrl(background_image, 600, 400)}
+        htmlWidth={300}
+        htmlHeight={200}
+      />
       <CardBody>
-        <HStack justifyContent={'space-between'} marginBottom={3}>
-          <PlatformIconList
-            platforms={parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={metacritic} />
-        </HStack>
+        {parent_platforms ? (
+          <HStack justifyContent={'space-between'} marginBottom={3}>
+            <PlatformIconList
+              platforms={parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={metacritic} />
+          </HStack>
+        ) : (
+          <Text>No platforms</Text>
+        )}
         <Heading fontSize={'2xl'}>{name}</Heading>
         <Emoji rating={rating_top} />
       </CardBody>
